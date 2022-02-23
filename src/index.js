e1c139ba6a9210c83678499d7f7ad800cac7596c
Vineeth <96077560+vineethevk@users.noreply.github.com>
2022-02-23 10:51:06 +0530

    update

---
+++ src/index.js
@@ -0,0 +1,18 @@
+const express = require("express");
+const connect = require("./config/db");
+
+const app = express();
+
+
+
+
+
+
+app.listen(2345, () => {
+    try {
+        console.log("listening 2345");
+    }catch(e){
+        console.log(e);
+    }
+    
+})
