29a537b76b1bac9532609e5221a6e82ed5d2823f
unnati1004 <95961486+unnati1004@users.noreply.github.com>
2022-02-24 14:04:02 +0530

    update

--- src/index.js
+++ src/index.js
@@ -1,18 +1,20 @@
 const express = require("express");
 const connect = require("./config/db");
-
+const product_controller = require("./controller/product_controller")
 const app = express();
 
-app.use()
+app.use(express.json());
+
+app.use("/nightwears",product_controller);
 
 
 
 app.listen(2345, () => {
     try {
-        connect();
+       connect();
         console.log("listening 2345");
     }catch(e){
-        console.log(e);
+        console.error({message:e.message});
     }
     
 })
