bf64bc01e524cccdd4e939a1f86b49e57e578c87
Omkarsalunkhe09 <ossalunkhe09@gmail.com>
2022-02-25 15:46:17 +0530

    dfhdfhd

--- src/index.js
+++ src/index.js
@@ -7,7 +7,9 @@ const app = express();
 app.use(express.json());
 
 app.use("/nightwears", product_controller);
-app.use("/tshirt",Tshirt_controller);
+app.use("/tshirt", Tshirt_controller);
+
+
 app.listen(2345, () => {
     try {
         connect();
