53a56d1de90e4159e5e004c0e22bc2aa27c48ea6
unnati1004 <95961486+unnati1004@users.noreply.github.com>
2022-02-25 15:36:53 +0530

    update

--- src/index.js
+++ src/index.js
@@ -1,13 +1,20 @@
 const express = require("express");
+const cors = require("cors");
 const connect = require("./config/db");
 const product_controller = require("./controller/nightwear_controller");
 const Tshirt_controller = require("./controller/Tshirt_controller");
 const thermal_controller = require("./controller/thermal_controller");
 
 const app = express();
-
 app.use(express.json());
 
+
+const corsOrigin ={
+    origin:'*', 
+    credentials:true,            //access-control-allow-credentials:true
+    optionSuccessStatus:200,
+ }
+ app.use(cors(corsOrigin));
 app.use("/nightwears", product_controller);
 app.use("/tshirt",Tshirt_controller);
 app.use("/thermal",thermal_controller);
